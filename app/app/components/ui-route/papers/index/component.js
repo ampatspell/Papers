8259baa3f98ac883136b02e4bbf27529364f2491
Arnis Vuskans <ampatspell@gmail.com>
2018-03-20 00:45:43 +0200

    minor app updates

--- app/app/components/ui-route/papers/index/component.js
+++ app/app/components/ui-route/papers/index/component.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
+import { getOwner } from '@ember/application';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-papers-index' ]
+  classNameBindings: [ ':ui-route-papers-index' ],
+
+  version: computed(function() {
+    return getOwner(this).factoryFor('config:environment').class.papers.version;
+  }).readOnly()
+
 });
